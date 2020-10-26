22a0cf9fc4022600818942d8558acdcf5d6cb586
Mark Pownell <MarkAlan@users.noreply.github.com>
2020-10-26 10:07:01 -0400

    completed claims work

--- src/ImageGallery.Client/Controllers/GalleryController.cs
+++ src/ImageGallery.Client/Controllers/GalleryController.cs
@@ -14,6 +14,7 @@ using System.Threading.Tasks;
 using System.Diagnostics;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using IdentityModel.Client;
 
 namespace ImageGallery.Client.Controllers
 { 
@@ -186,6 +187,43 @@ namespace ImageGallery.Client.Controllers
         {
             await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
             await HttpContext.SignOutAsync(OpenIdConnectDefaults.AuthenticationScheme);
+        }
+
+        [Authorize(Roles = "PayingUser")]
+        public async Task<IActionResult> OrderFrame()
+        {
+            var idpClient = _httpClientFactory.CreateClient("IDPClient");
+
+            var metaDataResponse = await idpClient.GetDiscoveryDocumentAsync();
+
+            if (metaDataResponse.IsError)
+            {
+                throw new Exception(
+                    "Problem accessing the discovery endpoint."
+                    , metaDataResponse.Exception);
+            }
+
+            var accessToken = await HttpContext
+              .GetTokenAsync(OpenIdConnectParameterNames.AccessToken);
+
+            var userInfoResponse = await idpClient.GetUserInfoAsync(
+               new UserInfoRequest
+               {
+                   Address = metaDataResponse.UserInfoEndpoint,
+                   Token = accessToken
+               });
+
+            if (userInfoResponse.IsError)
+            {
+                throw new Exception(
+                    "Problem accessing the UserInfo endpoint."
+                    , userInfoResponse.Exception);
+            }
+
+            var address = userInfoResponse.Claims
+                .FirstOrDefault(c => c.Type == "address")?.Value;
+
+            return View(new OrderFrameViewModel(address));
         }
 
         public async Task WriteOutIdentityInformation()
